0fa4f2695cb1fd5bea4061efb6da58f166acce6c
wahangga <wahangganteng@gmail.com>
2020-03-06 15:47:15 +0700

    deploy

---
+++ precache-manifest.81db94d85ef5394739e59139464e4925.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4774f5942528b54781dffbc9e01438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a90132dcf78f153587",
+    "url": "/static/css/main.90eb3dbb.chunk.css"
+  },
+  {
+    "revision": "c503f16d227289e6d1f9",
+    "url": "/static/js/2.55f4ec79.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.55f4ec79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a90132dcf78f153587",
+    "url": "/static/js/main.aaa33c7a.chunk.js"
+  },
+  {
+    "revision": "6974887d4d3a234560bc",
+    "url": "/static/js/runtime-main.35a8e492.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
